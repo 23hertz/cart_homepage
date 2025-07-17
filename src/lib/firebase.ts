25a6a06904e072617fd4cc1e7ae1c385ac3f68aa
Shakiru <Olaiya.adeolu@gmail.com>
2025-07-17 12:10:02 +0100

    adding of firebase to the cart project

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyb_HIz91FB5r0ZJpGVPfnq5ea0gSJx1A",
+  authDomain: "shoppingcart-3e51e.firebaseapp.com",
+  projectId: "shoppingcart-3e51e",
+  storageBucket: "shoppingcart-3e51e.firebasestorage.app",
+  messagingSenderId: "907558270213",
+  appId: "1:907558270213:web:690b39fd0191c42e1b336c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
